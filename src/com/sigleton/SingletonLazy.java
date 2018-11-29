6dd393eef9425165c0c9fc9162feab52781d97ea
Luodayu <Luodayu@DESKTOP-LPKCVBU>
2018-11-30 00:59:54 +0800

    singeton

---
+++ src/com/sigleton/SingletonLazy.java
@@ -0,0 +1,20 @@
+package com.sigleton;
+
+//懒汉式：创建对象实例的时候直接初始化
+public class SingletonLazy {
+	//1、创建类中私有构造
+	private SingletonLazy() {
+		
+	}
+	
+	//2、创建静态的该类实例对象
+	private static SingletonLazy instance;
+	
+	//3、创建开放的静态方法提供实例对象
+	public SingletonLazy getSingletonLazy() {
+		if (instance == null)
+			instance = new SingletonLazy();
+		return instance;
+	}
+
+}
